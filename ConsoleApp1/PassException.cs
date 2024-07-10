bf44a413f6c1e27b136def695c738d4c717cc7d1
RevanAga <revan.agazade.785@gmail.com>
2024-07-10 03:14:39 -0800

    1

---
+++ ConsoleApp1/PassException.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1;
+
+internal class PassException:Exception
+{
+    public string Message { get; set; }
+
+    public PassException(string message) : base(message)
+    {
+        Console.WriteLine(message);
+    }
+
+
+}
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,131 @@
+﻿using ConsoleApp1;
+
+class Program
+{
+    class Atm
+    {
+
+        int mebleg { get; set; }
+        string password { get; set; }
+
+        public Atm(int mebleg, string password)
+        {
+            this.mebleg = mebleg;
+            this.password = password;
+
+        }
+
+        bool Check()
+        {
+
+            Console.Write("Password: ");
+            string oldPass = Console.ReadLine();
+
+            return oldPass == password;
+        }
+
+        public void balans()
+        {
+            if (Check()) {
+                Console.WriteLine(mebleg);
+            }
+
+            else
+            {
+                throw new PassException("Wrong Password!!");
+            }
+        }
+
+        public void cashIn() {
+
+            if (Check())
+            {
+                Console.Write("Daxil Edilecek Mebleg: ");
+                int a = int.Parse(Console.ReadLine());
+                mebleg += a;
+
+                mebleg += a;
+            }
+
+            else
+            {
+                throw new PassException("Wrong Password!!");
+            }
+
+        }
+
+        public void cashOut()
+        {
+
+            if (Check())
+            {
+                Console.Write("Cixarilacaq Mebleg: ");
+                int b = int.Parse(Console.ReadLine());
+
+                if (b <= mebleg)
+                {
+                    mebleg -= b;
+                }
+
+                else { throw new PassException("Not Enogh Money"); }
+
+
+            }
+
+            else
+            {
+                throw new PassException("Wrong Password!!");
+            }
+
+        }
+
+        public void changePassword()
+        {
+
+
+            if (Check())
+            {
+               
+                Console.Write("New Password: ");
+                string newPass = Console.ReadLine();
+
+                password = newPass;
+
+            }
+
+            else
+            {
+                throw new PassException("Wrong Password!!");
+            }
+
+        }
+
+    }
+
+
+
+    static void Main(string[] args)
+    {
+
+        Atm atm = new Atm(200, "12345");
+
+        try
+        {
+            atm.cashIn();
+            atm.cashOut();
+            atm.balans();
+            atm.changePassword();
+            atm.cashIn();
+        }
+        catch (PassException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+    }
+
+}
